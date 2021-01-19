13cf6756cb3a4567b87de15a75df84875b6a0a70
TrickyPi <530257315@qq.com>
2021-01-19 20:55:23 +0800

    添加图片转化功能

--- bin/tnb.js
+++ bin/tnb.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
-const {program} = require('commander')
-const {version,description} = require('../package.json')
-const ip = require('../lib/ip.js')
-const gt = require('../lib/gt.js')
+const { program } = require("commander");
+const { version, description } = require("../package.json");
+const ip = require("../lib/ip.js");
+const gt = require("../lib/gt.js");
+const ct = require("../lib/ct.js");
 
-program
-    .version(version)
-    .description(description)
+program.version(version).description(description);
 
 // appear you ip
-program
-    .command('ip')
-    .description('appear your ip')
-    .action(ip)
+program.command("ip").description("appear your ip").action(ip);
 
 //composite image to sprite
 program
-    .command('gt')
-    .description('composite image to a sprite image')
-    .action(gt)
+    .command("gt")
+    .description("composite image to a sprite image")
+    .action(gt);
 
-program.parse(process.argv)
+//convert image
+program
+    .command("ct")
+    .description(
+        "convert image to a progressive image,scale image,include jpg jpeg png"
+    )
+    .action(ct);
+
+program.parse(process.argv);
