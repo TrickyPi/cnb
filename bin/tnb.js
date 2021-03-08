5cf28ce3c2bd0a7210ed1f73aad3783fda0b0b7c
TrickyPi <530257315@qq.com>
2021-03-08 22:09:21 +0800

    修改命令提示过慢

--- bin/tnb.js
+++ bin/tnb.js
@@ -2,20 +2,26 @@
 
 const { program } = require("commander");
 const { version, description } = require("../package.json");
-const ip = require("../lib/ip.js");
-const gt = require("../lib/gt.js");
-const ct = require("../lib/ct.js");
 
-program.version(version).description(description);
+program
+    .version(version)
+    .description(description);
 
 // appear you ip
-program.command("ip").description("appear your ip").action(ip);
+program
+    .command("ip")
+    .description("appear your ip")
+    .action(() => {
+        require("../lib/ip.js")()
+    });
 
 //composite image to sprite
 program
     .command("gt")
     .description("composite image to a sprite image")
-    .action(gt);
+    .action(() => {
+        require("../lib/gt.js")()
+    });
 
 //convert image
 program
@@ -23,6 +29,8 @@ program
     .description(
         "convert image to a progressive image,scale image,include jpg jpeg png"
     )
-    .action(ct);
+    .action((command) => {
+        require("../lib/ct.js")(command)
+    });
 
 program.parse(process.argv);
